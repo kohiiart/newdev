da1c59e5c72cc848ee7362ee48e7d9ef83199b97
Kohii <104533272+kohiiart@users.noreply.github.com>
2022-05-30 21:30:00 -0300

    Atualizando Formulário com JS
    Funções editar e remover mensagens

--- web/project-begginers/list-messages/main.js
+++ web/project-begginers/list-messages/main.js
@@ -1,5 +1,11 @@
 const buttonAddMessage = document.getElementById('add-button');
 
+let countRow = 0;
+
+const onClickEdit = (idRecord) => {
+    console.log('pei pei', idRecord);
+}
+
 function addMessage (event){
     event.preventDefault();
 
@@ -31,7 +37,7 @@ if (!textArea.value.length){
 
     console.log('-------oooo', message);
 
-    const sessionMessages = document.getElementById('section-messages');
+    /*const sessionMessages = document.getElementById('section-messages');
 
     let ul = sessionMessages.querySelector('ul');        //Uma Lista Não Ordenada(ul) é criada se a validação de sua existência for falsa
     if (!ul) {
@@ -39,12 +45,52 @@ if (!textArea.value.length){
         sessionMessages.appendChild(ul);
     }
     
-    document.getElementById('form-message').reset();
-    
     const li = document.createElement('li');
     li.innerHTML = `De: ${message.from} Para: ${message.to} Mensagem: ${message.message}`;                  //Crase
+    
+    ul.appendChild(li);             */
 
-    ul.appendChild(li);
+    document.getElementById('form-message').reset();
+    
+    let tbody = document.getElementById('tbody');
+
+    const tr = document.createElement("tr");
+
+    const tdFrom = document.createElement("td");
+    tdFrom.innerHTML = message.from;
+    
+    const tdTo = document.createElement("td");
+    tdTo.innerHTML = message.to;
+    
+    const tdMessage = document.createElement("td");
+    tdMessage.innerHTML = message.message;
+    
+    tr.appendChild(tdFrom);
+    tr.appendChild(tdTo);
+    tr.appendChild(tdMessage);
+    
+    const tdButtons = document.createElement('td');
+
+    
+
+    const iconEdit = document.createElement('i');
+    iconEdit.setAttribute('class','fas fa-edit');
+    tdButtons.appendChild(iconEdit);
+    iconEdit.setAttribute('style', 'cursor:pointer');
+    
+    const iconRemove = document.createElement('i');
+    iconRemove.setAttribute('class','fas fa-trash');
+    tdButtons.appendChild(iconRemove);
+    iconRemove.setAttribute('style', 'cursor:pointer');
+    
+    tr.appendChild(tdButtons);
+    
+    tr.setAttribute('id', countRow);
+    countRow += 1;
+    iconEdit.setAttribute('onclick', `onClickEdit(${tdButtons.parentElement.id});`)
+    console.log('pegar elemento pai', tdButtons.parentElement.id);
+
+    tbody.appendChild(tr);
 };
 
 buttonAddMessage.addEventListener('click', addMessage);

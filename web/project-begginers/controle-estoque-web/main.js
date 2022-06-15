49007cffe389c1a2a771389541d3585f8fb91e0d
Kohii <104533272+kohiiart@users.noreply.github.com>
2022-06-15 18:15:22 -0300

    consulta estoque web
    atualizando

--- web/project-begginers/controle-estoque-web/main.js
+++ web/project-begginers/controle-estoque-web/main.js
@@ -2,21 +2,32 @@ const storage = localStorage;
 
 let cars = [];
 
+let moviments = [];
+
+
 const onClickRemove = (element) => {
+   
+    
     const identificadorASerEncontrado = 
       +element.getAttribute('identificador');
-  console.log ('uepa ', identificadorASerEncontrado);
+  console.log ('uepaaaaa ', identificadorASerEncontrado);
     const carsDelete = loadCars();
      carsDelete.splice(identificadorASerEncontrado,1)
   
      localStorage.setItem('carRegistration', JSON.stringify(carsDelete));
-  
+      
+
     listCars();
   }
 
 function loadCars () {
     const cars = storage.getItem('carRegistration');
     return cars ? JSON.parse(cars) : [];
+}
+
+function loadMoviment () {
+    const moviments = storage.getItem('movimentRegistration');
+    return moviments ? JSON.parse(moviments) : [];
 }
 //Listar
 
@@ -36,26 +47,31 @@ const span = (identificador) => {
 
 const listCars = () => {
     const cars = loadCars();
-
-    let ul = document.querySelector('ul');
-  if (ul) {
-    ul.remove();
-  }
-  
-  ul = document.createElement('ul');
-
     
-    cars.forEach((item, identificador) => {
+    
+    function createUl (){
+       let ul = document.getElementById('list-cars-ul');
+       if (ul) {
+         document.getElementById('list-cars-section').appendChild(ul)
+        }   
+        ul = document.createElement('ul')
+        ul.setAttribute('id', `list-cars-ul`);
+   }
+       const ul = document.getElementById('list-cars-ul');
+   
+   cars.forEach((item, identificador) => {
         const li = document.createElement('li');
         li.innerHTML = `${item.model} ${item.brand} ${item.quantity}`;
-
+        
         li.appendChild(span(identificador))
-        console.log('item ====', item.model, item.brand, item.quantity)
+        li.setAttribute('id', `${identificador}`)
+        console.log('testndo-----', li.id)
+        
         if(ul){
-        ul.appendChild(li)
+            ul.appendChild(li)
         }
     }); 
-    
+    createUl()
 }
 
 
@@ -73,6 +89,7 @@ function registryCar(event) {
         brand: brand.value,
         quantity: +quantity.value
     }
+    
 
     cars = loadCars();
 
@@ -84,6 +101,64 @@ function registryCar(event) {
     console.log('----- ', car)
     listCars();
     document.querySelector('form').reset()
+}
+
+
+//-------------MOVIMENTO--------------------------------------------------------------------------------
+
+const listMoviment = () => {
+    const moviments = loadMoviment();
+    
+    
+    function createUlMoviment (){
+       let ul = document.getElementById('in-out-ul');
+       if (ul) {
+         document.getElementById('in-out-section').appendChild(ul)
+        }   
+        ul = document.createElement('ul')
+        ul.setAttribute('id', `in-out-ul`);
+   }
+       const ul = document.getElementById('in-out-ul');
+   
+   moviments.forEach((item, identificador) => {
+        const li = document.createElement('li');
+        li.innerHTML = `${item.enter} ${item.out}`;
+        console.log(item)
+        li.setAttribute('id', `${identificador}`)
+        console.log('testndo-----', li.id)
+        
+        if(ul){
+            ul.appendChild(li)
+        }
+    }); 
+    createUlMoviment()
+}
+
+
+listMoviment();
+
+function movimentRegistry(event) {
+    event.preventDefault();
+    console.log('movimentando')
+
+    const [enter, out] = document.getElementById('in-out-field').querySelectorAll('input');
+
+    const moviment = {
+        in: +enter.value,
+        out: +out.value        
+    }
+    
+
+    moviments = loadMoviments();
+
+    moviments.push(moviment)
+
+    storage.setItem('MovimentRegistration', JSON.stringify(moviment))
+
+
+    console.log('----- ', moviment)
+    listMoviment();
+    document.getElementById('in-out-form').querySelector('form').reset()
 }
 
 function pageList(){
@@ -99,4 +174,10 @@ const btnRegistry = document.getElementById('submit-register');
 
 if (btnRegistry) {
     btnRegistry.addEventListener('click', registryCar);
+}
+
+const btnMoviment = document.getElementById('submit-in-out');
+
+if (btnMoviment) {
+    btnMoviment.addEventListener('click', movimentRegistry)
 }

69946622cf625f10ecf3c395062e0161e530201a
Kohii <104533272+kohiiart@users.noreply.github.com>
2022-06-22 18:09:22 -0300

    to-do list
    To do list - treino

--- web/project-begginers/to-do-list/main.js
+++ web/project-begginers/to-do-list/main.js
@@ -5,6 +5,35 @@ const loadTasks = () =>{
     return todayTasks ? JSON.parse(todayTasks) : [];
 }
 
+const listTasks = () => {
+    const tasks = loadTasks();
+    const section = document.getElementById('tasks-list')
+
+    let divTasks = document.getElementById('tasks-div')
+    if (divTasks){
+        divTasks.remove();
+    }
+    divTasks = document.createElement('div')
+    divTasks.setAttribute('id', 'tasks-div')
+
+    tasks.forEach((item, index) => {
+        let input = document.createElement('input')
+        input.setAttribute('type', 'checkbox')
+        input.setAttribute('index', `${index}`)
+        let label = document.createElement('label')
+        label.setAttribute('for', `${index}`)
+        label.innerHTML = `${item}`
+
+        console.log('label----------->', label.getAttribute('index'))
+        divTasks.appendChild(label)
+
+        console.log('indice----------->', input.getAttribute('index'))
+        divTasks.appendChild(input)
+    });
+
+    section.appendChild(divTasks)
+}
+
 const addTask = (event) => {
     event.preventDefault();
     const task = document.getElementById('insert-task').value;
@@ -15,6 +44,8 @@ const addTask = (event) => {
     const todayTasks = loadTasks();
     console.log('add task------>', todayTasks);
     document.querySelector('form').reset();
+
+    listTasks();
 }
 
 const taskBtn = document.getElementById('submit-task')

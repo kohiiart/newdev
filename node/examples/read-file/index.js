bc6f95a4197651b2e791870b32fc84dfb0af4d47
Kohii <104533272+kohiiart@users.noreply.github.com>
2022-06-30 21:28:52 -0300

    node.js
    att

--- node/examples/read-file/index.js
+++ node/examples/read-file/index.js
@@ -1,19 +1,50 @@
 const http = require('http');
-const URL = require('url')
-const teams = require('./times.json');
+const URL = require('url');
+const fs = require('fs');
+const path = require('path');
+let users = require('./users.json');
 
-console.log('times---> ', teams)
+const getUsers = (request, response) => {
+  const { name, lastName, age, remove } = URL.parse(request.url, true).query;
+  let message = '';
+  if (name) {   
+    const user = {
+        name, lastName, age
+      } 
+    if (remove) {
+        message = 'Registro removido'
+        const found =  users.filter(user => String(user.name) == String(name));
+        users = users.filter(user => String(user.name) !== String(name));
 
-http.createServer((request, response)=>{
-    
-    const {nome, patrocinador} = URL.parse(request.url, true).query;
-    
-    if (nome || patrocinador){
-        console.log(nome, patrocinador);
+        if (!found.length){
+            return response.end('User not found')
+        }
+
+    } else {
+      users.push(user);
+      message = 'Registro salvo!'
     }
-    //responde pro cliente
-    response.end(JSON.stringify(teams))
+    fs.writeFile(
+      path.join(__dirname, 'users.json'),
+      JSON.stringify(users, null, 2),
+      (error) => {
+        if (error) return;
 
-}).listen(5679, () => {
-    console.log('API is running on port 5679')
+        console.log('Salvou o registro com sucesso');
+        response.end(JSON.stringify({
+            status: message,
+            data: user, 
+        }
+        ));
+      }
+    );
+  } else{
+    response.end(JSON.stringify(users));
+  }
+}
+
+const server = http.createServer(getUsers);
+
+server.listen(3001, () => {
+  console.log('API listening on http://localhost:3001');
 });
